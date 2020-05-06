2aa88ee35ebd45179b368672eac89b23f65b4c46
Vlad Vasiliu <code@vladvasiliu.com>
2020-05-06 09:32:49 +0200

    Added more error cases to Authorize Ingress Error.

--- src/aws/error.rs
+++ src/aws/error.rs
@@ -53,6 +53,8 @@ impl From<RusotoError<DescribeInstancesError>> for InstanceError {
 #[derive(Debug)]
 pub enum SGAuthorizeIngressError {
     DuplicateRule(HttpResponseDescription),
+    MalformedRule(HttpResponseDescription),
+    RuleLimitExceeded(HttpResponseDescription),
     UnknownHttpError(HttpResponseDescription),
     Unknown(RusotoError<AuthorizeSecurityGroupIngressError>),
 }
@@ -63,6 +65,8 @@ impl fmt::Display for SGAuthorizeIngressError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
             Self::DuplicateRule(err) => write!(f, "{}", err),
+            Self::MalformedRule(err) => write!(f, "{}", err),
+            Self::RuleLimitExceeded(err) => write!(f, "{}", err),
             Self::UnknownHttpError(err) => write!(f, "{}", err),
             Self::Unknown(err) => write!(f, "Unknown error: {}", err),
         }
@@ -84,20 +88,30 @@ impl From<HttpResponseDescription> for SGAuthorizeIngressError {
     fn from(err: HttpResponseDescription) -> Self {
         // If status is 403, it should be handled by a more generic error
         // If status is other than 400, we don't know what it is
-        if err.status == 400 {
-            // If there is no error, we can't do anything
-            // There shouldn't be several errors
-            // wf there are, the first one is probably the one we want
-            return match err.errors.first() {
-                Some(error_detail)
-                    if error_detail.code == Some(String::from("InvalidPermission.Duplicate")) =>
-                {
-                    Self::DuplicateRule(err)
-                }
-                _ => Self::UnknownHttpError(err),
-            };
+        if err.status != 400 {
+            return Self::UnknownHttpError(err);
         }
-        Self::UnknownHttpError(err)
+
+        // If there is no error, we can't do anything
+        // There shouldn't be several errors
+        // if there are, the first one is probably the one we want
+        let error_detail = match err.errors.first() {
+            None => return Self::UnknownHttpError(err),
+            Some(error_detail) => error_detail,
+        };
+
+        match error_detail.code.as_deref() {
+            Some("InvalidPermission.Duplicate") => Self::DuplicateRule(err),
+            Some("InvalidPermission.Malformed") => Self::MalformedRule(err),
+            Some("RulesPerSecurityGroupLimitExceeded") => return Self::RuleLimitExceeded(err),
+            _ => Self::UnknownHttpError(err),
+        }
+
+        // if error_detail.code == Some(String::from("InvalidSecurityGroupID.NotFound")) {
+        //     return Self::GroupNotFound(err);
+        // }
+
+        // Self::UnknownHttpError(err)
     }
 }
 
@@ -107,6 +121,7 @@ pub enum SecurityGroupError {
     ReturnedNone,
     DescribeError(HttpResponseDescription),
     AuthorizeIngressError(SGAuthorizeIngressError),
+    NotFound(HttpResponseDescription),
     UnknownError(Box<dyn Error>),
 }
 
@@ -119,6 +134,7 @@ impl fmt::Display for SecurityGroupError {
             Self::ReturnedTooMany => write!(f, "too many instances returned"),
             Self::DescribeError(err) => write!(f, "failed to get security group: {}", err),
             Self::AuthorizeIngressError(err) => write!(f, "failed to authorize ip: {}", err),
+            Self::NotFound(err) => write!(f, "{}", err),
             Self::UnknownError(err) => write!(f, "{}", err),
         }
     }
